9f3bd87b72ec76708791e02080a2478fdd452776
[kaylmatyu] <[calingasankylematthew@gmail.com]>
2024-10-30 07:10:59 +0800

    tinanggal ko lnag mga sobra

--- landing_page.php
+++ landing_page.php
@@ -6,7 +6,6 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Dine&Watch</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link rel='stylesheet' type='text/css' media='screen' href='css/Lpage.css'>
 </head>
